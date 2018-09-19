e251c1952238f591e8835c5ce9e0eedf008980ff
gsagariw <gsagariw@gmail.com>
2018-09-19 16:47:51 +0545

    Add functionality to the buttons and inputs

--- app/src/main/java/com/insightworkshop/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/insightworkshop/dinnerdecider/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.insightworkshop.dinnerdecider
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chinese", "Hamburger", "Sandwich", "Pizza", "Thakali Khana")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        selectedFoodText.text = "Burger King"
+        decideButton.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodText.text = foodList[randomFood]
+        }
+
+        addFoodButton.setOnClickListener {
+            val newFood = addFoodText.text.toString()
+            foodList.add(newFood)
+            addFoodText.text.clear()
+        }
 
     }
 }
