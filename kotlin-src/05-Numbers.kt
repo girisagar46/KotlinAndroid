c990f35fa53308762815fa163f041d8be103d32e
gsagariw <gsagariw@gmail.com>
2018-09-19 18:00:40 +0545

    Arithmetic in Kotlin

---
+++ kotlin-src/05-Numbers.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val pi = 3.1415
+    val radius = 2.3
+
+    println(pi*radius*radius)
+    println(pi+radius)
+    println(pi-radius)
+    println(pi/radius)
+
+    println(12%5)
+    // println(5/0) // throws: java.lang.ArithmeticException: / by zero
+
+}
