db20a959ed19e3aeefcd938025f422449ac7cde8
gsagariw <gsagariw@gmail.com>
2018-09-19 17:31:37 +0545

    Variables in Kotlin

--- kotlin-src/02-Variables.kt
+++ kotlin-src/02-Variables.kt
@@ -1,5 +1,8 @@
 fun main(args: Array<String>) {
 
+    /*
+    * Kotlin has type inference so that we need not have to mention the type of variable
+    * */
 
     var x = 10
     var name1 = "Sagar"
@@ -14,5 +17,42 @@ fun main(args: Array<String>) {
 
     println(test)
     // test = "New String" // Can not do this.
-    print(test+name2) // String concatenation
+    println(test+name2) // String concatenation
+
+
+    var name = "Sagar Giri"
+    var isAwesome = true
+
+    println("Is $name awesome? The answer is : $isAwesome")
+
+
+    var a = 3
+    var b = 5
+    println(a+b)
+
+    val myName = "Sagar Giri" // myName is immutable
+    var amIAwesome = true   // amIAwesome is mutable
+
+    println("Is $myName awesome? The answer is : $amIAwesome")
+
+    val firstFloat = 3.1415 // 64-bit
+    val secondFloat = 3.1415f // 32-bit
+    println(firstFloat + secondFloat)
+
+    val longNum = 123456789456456L // 64-bit
+    println(longNum)
+
+    val aDouble = a.toDouble()
+    print(aDouble)
+
+    // To explicitly set types in the variable (Which is not necessary. Do this.
+    val aa: Double = 3.1412
+    val bb: Float = 3.1412f
+    var cc: Long = 521425454L
+    var dd: String = "Hello World"
+
+    var hero: String? // ? is for to tell that it is empty variable
+    hero = "Batman"
+    println(hero)
+
 }
